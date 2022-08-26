cb9da4b7a7adc7b1d3870382636d5d3da4a924b1
Chen Pan <chen.pan@goat.com>
2022-08-26 16:20:16 +0800

    defined statusbarwidget.

--- src/main/kotlin/me/bytebeats/ipp/worldclock/Consts.kt
+++ src/main/kotlin/me/bytebeats/ipp/worldclock/Consts.kt
@@ -2,3 +2,12 @@ package me.bytebeats.ipp.worldclock
 
 const val WORLD_CLOCK_WIDGET_ID_1 = "world-clock-widget-id-1"
 const val WORLD_CLOCK_WIDGET_ID_2 = "world-clock-widget-id-2"
+
+const val WORLD_CLOCK_DEFAULT_TIME_ZONE_1 = "Asia/Shanghai"
+const val WORLD_CLOCK_DEFAULT_TIME_ZONE_2 = "America/Montreal"
+
+const val CP_KEY_CLOCK_TIME_ZONE_1 = "CP_KEY_CLOCK_TIME_ZONE_1"
+const val CP_KEY_CLOCK_TIME_ZONE_2 = "CP_KEY_CLOCK_TIME_ZONE_2"
+const val CP_KEY_CLOCK_ENABLE_1 = "CP_KEY_CLOCK_ENABLE_1"
+const val CP_KEY_CLOCK_ENABLE_2 = "CP_KEY_CLOCK_ENABLE_2"
+const val CP_KEY_USE_24H_TIME_FORMAT = "CP_KEY_USE_24H_TIME_FORMAT"
---
+++ src/main/kotlin/me/bytebeats/ipp/worldclock/DigitalClockStatusWidget.java
@@ -0,0 +1,78 @@
+package me.bytebeats.ipp.worldclock;
+
+import com.intellij.ide.util.PropertiesComponent;
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.wm.StatusBar;
+import com.intellij.openapi.wm.StatusBarWidget;
+import com.intellij.openapi.wm.WindowManager;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NonNls;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class DigitalClockStatusWidget implements StatusBarWidget {
+    private final String widgetID;
+    private final StatusBar statusBar;
+
+    private final PropertiesComponent pc = PropertiesComponent.getInstance();
+
+    private Timer timer;
+
+    @Contract(pure = true)
+    public DigitalClockStatusWidget(String widgetID, Project project) {
+        this.widgetID = widgetID;
+        statusBar = WindowManager.getInstance().getStatusBar(project);
+    }
+
+
+    @Override
+    public @NonNls
+    @NotNull String ID() {
+        return widgetID;
+    }
+
+    @Override
+    public void install(@NotNull StatusBar statusBar) {
+        ApplicationManager.getApplication().executeOnPooledThread(() -> startClockIfNot(statusBar));
+    }
+
+    private void startClockIfNot(StatusBar statusBar) {
+        if (ConstsKt.WORLD_CLOCK_WIDGET_ID_1.equals(widgetID) && pc.getBoolean(ConstsKt.CP_KEY_CLOCK_ENABLE_1, true)
+                || ConstsKt.WORLD_CLOCK_WIDGET_ID_2.equals(widgetID) && pc.getBoolean(ConstsKt.CP_KEY_CLOCK_ENABLE_2, true)) {
+            try {
+                timer = new Timer();
+                timer.schedule(new TimerTask() {
+                    @Override
+                    public void run() {
+                        statusBar.updateWidget(widgetID);
+                    }
+                }, 0L, 30000L);
+            } catch (Exception ignore) {
+                // do nothing here
+            }
+        }
+    }
+
+    public void refresh() {
+        dispose();
+        startClockIfNot(statusBar);
+    }
+
+    @Override
+    public void dispose() {
+        if (timer != null) {
+            timer.cancel();
+            timer.purge();
+            timer = null;
+        }
+    }
+
+    @Override
+    public @Nullable WidgetPresentation getPresentation() {
+        return StatusBarWidget.super.getPresentation();
+    }
+}
